19b8c85f6d8a41df55f92dae600b5aba90bb07e9
Edgar Eduardo Nuñez Gonzalez <edgar_v23@hotmail.com>
2017-03-14 00:56:16 -0700

    dos

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,9 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+Route::get('prueba',function(){
+	return "hola desde php";
+});
 
 Route::get('/', 'WelcomeController@index');
 
